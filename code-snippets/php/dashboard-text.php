61a6b4299f007f3c9d6489c0e5c652ab79df58ed
bueroblanko <49558119+bueroblanko@users.noreply.github.com>
2024-10-24 13:15:45 +0200

    Update dashboard-text.php

--- code-snippets/php/dashboard-text.php
+++ code-snippets/php/dashboard-text.php
@@ -9,5 +9,5 @@ wp_add_dashboard_widget('custom_help_widget', 'Ansprechpartner bei Fragen', 'cus
 }
 
 function custom_dashboard_help() {
-echo '  <img src="/wp-content/uploads/2024/10/bb-template-logo.svg" alt="Philipp Ludwig" style="max-width: 50px; border-radius: 100%; height: auto; margin-bottom: 20px;"></br><p><span style="color: black;font-size: 14px;"><strong>&copy; Büro Blanko Medien GmbH</strong></span><br>Telefon: <a style="text-decoration:none;color:#000;" href="tel:062144575895">+49 621 445 758 95</a><br><a href="https://www.bueroblanko.de">www.bueroblanko.de</a> | <a href="mailto:info@bueroblanko.de">info@bueroblanko.de</a></p>';
+echo '  <img src="/wp-content/plugins/buero-blanko-basic/includes/img/bb-template-logo.svg" alt="Philipp Ludwig" style="max-width: 50px; border-radius: 100%; height: auto; margin-bottom: 20px;"></br><p><span style="color: black;font-size: 14px;"><strong>&copy; Büro Blanko Medien GmbH</strong></span><br>Telefon: <a style="text-decoration:none;color:#000;" href="tel:062144575895">+49 621 445 758 95</a><br><a href="https://www.bueroblanko.de">www.bueroblanko.de</a> | <a href="mailto:info@bueroblanko.de">info@bueroblanko.de</a></p>';
 }

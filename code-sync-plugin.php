7d7a0f7dfac43a4d5d9d4ff89897b6ff4eda65d4
bueroblanko <info@bueroblanko.de>
2025-09-22 23:00:23 +0200

    Update code-sync-plugin.php

--- code-sync-plugin.php
+++ code-sync-plugin.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Büro Blanko Basic
  * Plugin URI:        https://bueroblanko.de
  * Description:       to sync all code snippets across client sites
- * Version:           0.0.6
+ * Version:           0.0.7
  * Author:            Büro Blanko Medien GmbH
  * Author URI:        https://bueroblanko.de
  * License:           GPL-2.0+
